64b0eda5f543964bfc7042b05a36dac524f6c575
Tyler Harris <laser505t@gmail.com>
2019-08-17 14:13:13 -0400

    Fix the Tuesday not showing up

--- src/Components/pages/About/index.js
+++ src/Components/pages/About/index.js
@@ -69,7 +69,7 @@ class About extends Component {
                   </p>
                   <p>
                     <b>Weekly meetings: </b>
-                    7:00 - 8:00PM in JR 270
+                    Tuesdays at 7:00 - 8:00PM in JR 270
                     <br />
                   </p>
                   <p>
