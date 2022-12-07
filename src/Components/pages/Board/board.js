acfe2654d0fd88022b9288fded0e2982f1c34a14
Lennon Anderson <renedes@protonmail.com>
2022-12-07 17:26:40 -0500

    Add shannon

--- src/Components/pages/Board/board.js
+++ src/Components/pages/Board/board.js
@@ -24,14 +24,14 @@ export default [
   },
   {
     active: 'info',
-    actionKey: '',
-    name: '',
-    image: 'img/board/unknown.png',
+    actionKey: 'sm',
+    name: 'Shannon McKinney',
+    image: 'img/board/shannon.jpg',
     role: 'Treasurer',
-    email: '',
-    bio: ``,
-    quote: ``,
-    linkedin: '',
-    github: ''
+    email: 'mckinney.409@osu.edu',
+    bio: `Irish dancer, computer programmer, haver of fun`,
+    quote: `“Life isn’t about waiting for the storm to pass, it’s about learning to dance in the rain” -Vivian Greene`,
+    linkedin: 'https://www.linkedin.com/in/shannon-m-74aa11166/',
+    github: 'https://github.com/ShannonMcKinney1'
   }
 ]
